4acfb92053f0442315c9845346c8e57b0f68da6f
Josh Steele <jjsteele@ualberta.ca>
2020-12-10 14:57:36 -0700

    Added the Albertasaurus hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,29 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Img from "gatsby-image"
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <Layout>
       {/*<SEO title="Josh's Resume Site" />*/}
-
+      <Img
+          fluid={data.albertasaurus.childImageSharp.fluid}
+          css={css`
+                width: 100%;
+            `}
+      />
   </Layout>
 )
+export const query = graphql`
+    query {
+        albertasaurus: file(relativePath: { eq: "Albertasaurus.png" }) {
+            childImageSharp {
+                fluid {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+    }
+`
 
 export default IndexPage
