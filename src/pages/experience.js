9ed42578f33cda9df2b6c8687a8f3684c825e653
Josh <34256170+ScienceOfSleep@users.noreply.github.com>
2021-02-12 14:18:32 -0700

    Rough mobile outline for the big experience cards. Name under review lol

--- src/pages/experience.js
+++ src/pages/experience.js
@@ -39,7 +39,14 @@ const ExperiencePage = ({data}) => {
                     css={css`
                       border: 5px solid var(--page-color);
                       border-radius: 10px;
+                      background-color: #ffffff;
                       filter: drop-shadow(4px 4px 4px hsl(202, 21%, 52%, 0.9));
+                      display: grid;
+                      grid-template-columns: 5px 1fr 5px;
+                      justify-items: center;
+                      *{
+                        grid-column: 2;
+                      }
                       @media screen and (min-width: 1120px){
                         grid-column: 2/4 !important;
                       }
@@ -49,23 +56,35 @@ const ExperiencePage = ({data}) => {
                         fluid={data.dip.childImageSharp.fluid}
                         alt="Draw It Paint"
                         css={css`
-                          width: 80%;
+                          width: 150px;
                           @media screen and (min-width: 1120px){
-                            width: 50%;
                           }
                         `}
                     />
-                    <div>
-                        <SectionHeading title="Draw It Paint"/>
-                        <h3>
-                            February 2020 - Present
-                        </h3>
-                        <p>
-                            This is the overview/elevator pitch page. I’ll give you a quick rundown on what I’ve done and what I prioritise while the site will give you an idea of what I’m like and what I can do.
-                            <br/> <br/>
-                            When sorting through applicants, you need to kiss a lot of frogs to find your metaphorical prince, or really even a decent looking frog. I appreciate your time here, so I’ll limit any long form text content to (appropriately flagged) sections on pages further in.
-                        </p>
+                    <div
+                        className="titleBox"
+                        css={css`
+                          display: flex;
+                          flex-direction: column;
+                          align-items: center;
+                        `}
+                    >
+                    <SectionHeading title="Draw It Paint"/>
+                    <h3 css={css`text-align: center`}>
+                        Web Developer
+                    </h3>
+                    <h3 css={css`text-align: center`}>
+                        February 2020 - Present
+                    </h3>
                     </div>
+                    <p css={css`margin: 5px 0 10px`}>
+                        This is the overview/elevator pitch page. I’ll give you a quick rundown on what I’ve done and what I prioritise while the site will give you an idea of what I’m like and what I can do.
+                    </p>
+                    <ul>
+                        <li>Building/maintaining eCommerce sites in GatsbyJS & Shopify</li>
+                        <li>Coordinating graphics work to meet site, project, and social media specifications</li>
+                        <li>Internal workflow optimization and automation</li>
+                    </ul>
                 </div>
             </BodyFold>
         </main>
@@ -84,6 +103,13 @@ export const query = graphql`
         landingDivider: file(relativePath: { eq: "experience-circle-divider-0.599393272614402.png" }) {
             childImageSharp {
                 fluid(quality: 100) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+        dip: file(relativePath: { eq: "dip-logo.png" }) {
+            childImageSharp {
+                fluid(quality:100) {
                     ...GatsbyImageSharpFluid
                 }
             }
