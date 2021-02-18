571bf4d99e49e18d066f54f072ae3f2f04a5a4f2
Josh <34256170+ScienceOfSleep@users.noreply.github.com>
2021-02-17 20:49:44 -0700

    Footer color and social logos added

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,40 +3,29 @@ import Navbar from "./navbar"
 import Img from "gatsby-image"
 import { css } from "@emotion/core"
 import "@fontsource/ubuntu"
+import GithubSVG from "./github-svg";
+import LinkedInSVG from "./linkedin-svg";
 
 
 const Footer = () => {
 
     return <footer
         css={css`
-              background-color: #2F4B5C;
+              background-color: #4A434A;
               color: #ffffff;
               display: grid;
-              grid-template-columns: minmax(10%, var(--horizontal-margin)) 1fr 1fr minmax(10%, var(--horizontal-margin));
+              grid-template-columns: minmax(10%, var(--horizontal-margin)) 1fr minmax(10%, var(--horizontal-margin));
               align-items: center;
               font-family: 'Ubuntu', sans-serif;
-              grid-column: 3;
               height: var(--header-height);
-              box-shadow: 0 4px 4px hsl(202, 21%, 52%, 0.6);
+              box-shadow: 0 -4px 4px hsl(202, 21%, 52%, 0.6);
+              *{
+                grid-column: 2;
+              }
             `}
     >
-        <div
-            css={css`
-              display: flex;
-              grid-column: 2;
-              align-items: center;
-            `}
-        >
-            <h2
-                css={css`
-                  padding-left: 1rem;
-                  font-size: 27px;
-              `}
-            >
-                Josh Steele's<br/>Resume Site
-            </h2>
-        </div>
-        <Navbar/>
+        <GithubSVG/>
+        <LinkedInSVG/>
     </footer>
 }
 
