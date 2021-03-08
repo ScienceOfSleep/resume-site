85ccb15c367c02f7336563358ac9973c91807540
Josh <34256170+ScienceOfSleep@users.noreply.github.com>
2021-03-08 13:45:40 -0700

    Skills List mobile formatting

--- src/components/skills-list.js
+++ src/components/skills-list.js
@@ -2,14 +2,16 @@ import React, {useState} from "react";
 import styled from "@emotion/styled";
 
 const SkillsList = styled.li`
-  border-top: 4px solid var(--page-color);
-  border-bottom: 4px solid var(--page-color);
+  border-right: 4px solid var(--page-color);
+  border-left: 4px solid var(--page-color);
+  margin: 1rem 0;
   width: fit-content;
   align-self: center;
   justify-self: center;
   list-style: none;
   padding: 0;
   font-family: 'montserrat', sans-serif;
+  text-align: center;
   font-weight: 600;
   li{
     padding: 3px 5px;
@@ -18,6 +20,11 @@ const SkillsList = styled.li`
   @media screen and (min-width: 1120px){
     grid-column: 3 !important;
     grid-row: 3;
+    border-top: 4px solid var(--page-color);
+    border-bottom: 4px solid var(--page-color);
+    border-right: 0;
+    border-left: 0;
+    text-align: left;
   }
 `
 
